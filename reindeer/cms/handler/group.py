c2ac8965ef2850bfc817e4eb68495743ba2edfe3
cuizhe01 <cuizhe01@baidu.com>
2015-10-20 10:36:24 +0800

    151020 : add cms group module

---
+++ reindeer/cms/handler/group.py
@@ -0,0 +1,96 @@
+__author__ = 'CuiVincent'
+# -*- coding: utf8 -*-
+
+from tornado.escape import json_encode
+import reindeer.base.base_handler
+from reindeer.cms.model.cms_group import CmsGroup
+from reindeer.cms.exceptions import CmsBusinessRuleException
+from reindeer.cms.model.cms_group_user import CmsGroupUser
+
+
+class GroupListHandler(reindeer.base.base_handler.BaseHandler):
+    def get(self):
+        self.render('cms/page/group/group_list.html')
+
+    def post(self):
+        json = CmsGroup.get_all_json()
+        r_json = '{"success": true, "aaData":' + json + '}'
+        print(r_json)
+        return self.write(r_json)
+
+
+class GroupListUserJoinedHandler(reindeer.base.base_handler.BaseHandler):
+    def post(self):
+        uid = self.get_argument('uid')
+        json = CmsGroup.get_json_by_joined_user(uid)
+        r_json = '{"success": true, "aaData":' + json + '}'
+        print(r_json)
+        return self.write(r_json)
+
+
+class GroupListUserUnjoinedHandler(reindeer.base.base_handler.BaseHandler):
+    def post(self):
+        uid = self.get_argument('uid')
+        json = CmsGroup.get_json_by_unjoined_user(uid)
+        r_json = '{"success": true, "aaData":' + json + '}'
+        print(r_json)
+        return self.write(r_json)
+
+
+class GroupAddHandler(reindeer.base.base_handler.BaseHandler):
+    def post(self):
+        name = self.get_argument('name')
+        des = self.get_argument('des')
+        err_code = CmsGroup.add(name, des, self.get_current_user().CODE)
+        if err_code == 0:
+            return self.write(json_encode({'success': True}))
+        else:
+            raise CmsBusinessRuleException(err_code)
+
+
+class GroupDeleteHandler(reindeer.base.base_handler.BaseHandler):
+    def post(self):
+        gids = str(self.get_argument('gid')).split(',')
+        success_count = 0
+        for gid in gids:
+            err_code = CmsGroup.delete(gid)
+            if err_code == 0:
+                success_count += 1
+        if success_count > 0:
+            return self.write(json_encode({'success': True}))
+        else:
+            raise CmsBusinessRuleException(err_code)
+
+
+class GroupEditHandler(reindeer.base.base_handler.BaseHandler):
+    def post(self):
+        gid = self.get_argument('gid')
+        name = self.get_argument('name')
+        des = self.get_argument('des')
+        err_code = CmsGroup.update(gid, name, des)
+        if err_code == 0:
+            return self.write(json_encode({'success': True}))
+        else:
+            raise CmsBusinessRuleException(err_code)
+
+
+class GroupUserAddHandler(reindeer.base.base_handler.BaseHandler):
+    def post(self):
+        uid = str(self.get_argument('uid')).split(',')
+        gid = str(self.get_argument('gid')).split(',')
+        err_code = CmsGroupUser.add(gid, uid)
+        if err_code == 0:
+            return self.write(json_encode({'success': True}))
+        else:
+            raise CmsBusinessRuleException(err_code)
+
+
+class GroupUserDeleteHandler(reindeer.base.base_handler.BaseHandler):
+    def post(self):
+        uid = str(self.get_argument('uid')).split(',')
+        gid = str(self.get_argument('gid')).split(',')
+        err_code = CmsGroupUser.delete(gid, uid)
+        if err_code == 0:
+            return self.write(json_encode({'success': True}))
+        else:
+            raise CmsBusinessRuleException(err_code)
--- reindeer/cms/urls.py
+++ reindeer/cms/urls.py
@@ -3,6 +3,7 @@ __all__ = ['cms_urls', 'cms_modules']
 
 from reindeer.cms.handler import app
 from reindeer.cms.handler import user
+from reindeer.cms.handler import group
 
 cms_urls = [
     (r'/cms/app_list', app.AppListHandler),
@@ -11,7 +12,15 @@ cms_urls = [
     (r'/cms/user_list_group_unjoined', user.UserListGroupUnjoinedHandler),
     (r'/cms/user_add', user.UserAddHandler),
     (r'/cms/user_edit', user.UserEditHandler),
-    (r'/cms/user_delete', user.UserDeleteHandler)
+    (r'/cms/user_delete', user.UserDeleteHandler),
+    (r'/cms/group_list', group.GroupListHandler),
+    (r'/cms/group_list_user_joined', group.GroupListUserJoinedHandler),
+    (r'/cms/group_list_user_unjoined', group.GroupListUserUnjoinedHandler),
+    (r'/cms/group_add', group.GroupAddHandler),
+    (r'/cms/group_delete', group.GroupDeleteHandler),
+    (r'/cms/group_edit', group.GroupEditHandler),
+    (r'/cms/group_user_add', group.GroupUserAddHandler),
+    (r'/cms/group_user_delete', group.GroupUserDeleteHandler),
 ]
 
 cms_modules = {}
